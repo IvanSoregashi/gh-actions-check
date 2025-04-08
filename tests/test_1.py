c59d1a7587cf44239fc642da5319b13c34c6563b
IvanSoregashi <ivanshiriaev2222@gmail.com>
2025-04-08 21:23:33 +0500

    git push

--- tests/test_1.py
+++ tests/test_1.py
@@ -5,8 +5,17 @@ def test_3():
     assert True
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def test_34():
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--remote-allow-origins=*")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--window-size=1920,1080")
+    driver = webdriver.Chrome(options=chrome_options)
     driver.get("http://example.com")
+
     assert driver.current_url == "http://example.com"
