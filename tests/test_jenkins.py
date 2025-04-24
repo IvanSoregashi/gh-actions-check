439d541205ab3a775a0aa50ee72b96896c502ee8
IvanSoregashi <ivanshiriaev2222@gmail.com>
2025-04-24 07:15:57 +0500

    to the project state

--- tests/test_jenkins.py
+++ tests/test_jenkins.py
@@ -1,37 +1,7 @@
-import time
-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-
-import os
-import pytest
-
-url = "http://" + os.getenv("JENKINS_HOST") + ":" + os.getenv("JENKINS_PORT") + "/"
+def test_login(login_page):
+    assert login_page.title == "Sign in [Jenkins]"
 
 
-@pytest.fixture
-def driver():
-    chrome_options = Options()
-    chrome_options.add_argument("--remote-allow-origins=*")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--headless=new")
-    chrome_options.add_argument("--window-size=1920,1080")
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get(url)
-    driver.find_element(By.NAME, "j_username").send_keys(os.getenv("JENKINS_USERNAME"))
-    driver.find_element(By.NAME, "j_password").send_keys(os.getenv("JENKINS_PASSWORD"))
-    driver.find_element(By.NAME, "Submit").click()
-    time.sleep(2)
-    yield driver
-    driver.quit()
+def test_main_page(main_page):
+    assert main_page.title == "Dashboard [Jenkins]"
 
-def test_jenkins(driver):
-    assert driver.current_url == "http://localhost:8080/"
-
-def test_item(driver):
-    time.sleep(63)
-    element = driver.find_element(By.LINK_TEXT, "Test item")
-    assert element.get_attribute("href") == "http://localhost:8080/job/Test%20item/"
